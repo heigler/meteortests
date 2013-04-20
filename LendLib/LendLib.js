8913f52f5dd7cd25b18feb2a6446b0c781aa2f31
Heigler Rocha <lordheigler@gmail.com>
2013-04-19 22:46:04 -0300

    adding items to LendLib

--- LendLib/LendLib.js
+++ LendLib/LendLib.js
@@ -3,7 +3,7 @@ lists = new Meteor.Collection('Lists');
 if (Meteor.isClient) {
   Template.categories.lists = function () {
     return lists.find({}, {sort: {Category: 1}});
-  }
+  };
 
   // flag para mostrar ou não o campo #add-category
   Session.set('adding_category', false);
@@ -11,7 +11,7 @@ if (Meteor.isClient) {
   // seta a flag em uma variável de template new_cat
   Template.categories.new_cat = function () {
     return Session.equals('adding_category', true);
-  }
+  };
 
   Template.categories.events({
     'click #btnNewCat': function (e, t) {
@@ -45,6 +45,43 @@ if (Meteor.isClient) {
     i.focus();
     i.select();
   }
+
+  Template.list.items = function () {
+    if (Session.equals('current_list', null)) {
+      return null;
+    }else{
+      var cats = lists.findOne({_id:Session.get('current_list')});
+      if (cats && cats.items) {
+        for (var i = 0; i < cats.items.length; i++) {
+          var d = cats.items[i];
+          d.Lendee = d.LentTo ? d.LentTo : 'free';
+          d.LendClass = d.LentTo ? 'label-important' : 'label-success';
+        }
+        return cat.items;
+      }
+    }
+  };
+
+  Template.list.list_selected = function () {
+    return ((Session.get('current_list') != null) &&
+      (!Session.equals('current_list'), null));
+  };
+
+  Template.categories.list_status = function () {
+    if (Session.equals('current_list', this._id)) {
+      return '';
+    }else{
+      return ' btn-inverse';
+    }
+  };
+
+  Template.list.list_adding = function () {
+    return (Session.equals('list_adding', true));
+  };
+
+  Template.list.lendee_editing = function () {
+    return (Session.equals('lendee_input', this.Name));
+  };
 }
 
 if (Meteor.isServer) {
